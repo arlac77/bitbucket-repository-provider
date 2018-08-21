6ab970cca95994d31337b2891f810e3c5932650e
Markus Felten <Markus.Felten@gmx.de>
2018-08-22 00:14:41 +0200

    test: optionsFromEnvironment* tests are not async

--- tests/bitbucket-provider-test.js
+++ tests/bitbucket-provider-test.js
@@ -1,204 +1,204 @@
-import test from 'ava';
-import { BitbucketProvider } from '../src/bitbucket-provider';
+import test from "ava";
+import { BitbucketProvider } from "../src/bitbucket-provider";
 
 const REPOSITORY_URL =
-  'https://arlac77@bitbucket.org/arlac77/sync-test-repository.git';
-const REPOSITORY_NAME = 'arlac77/sync-test-repository';
+  "https://arlac77@bitbucket.org/arlac77/sync-test-repository.git";
+const REPOSITORY_NAME = "arlac77/sync-test-repository";
 
 const config = BitbucketProvider.optionsFromEnvironment(process.env);
 
-test('optionsFromEnvironment undefined', async t => {
+test("optionsFromEnvironment undefined", t => {
   t.is(BitbucketProvider.optionsFromEnvironment(undefined), undefined);
 });
 
-test('optionsFromEnvironment user', async t => {
+test("optionsFromEnvironment user", t => {
   t.deepEqual(
     BitbucketProvider.optionsFromEnvironment({
-      BITBUCKET_USERNAME: 'user',
-      BITBUCKET_PASSWORD: 'pass'
+      BITBUCKET_USERNAME: "user",
+      BITBUCKET_PASSWORD: "pass"
     }),
-    { auth: { type: 'basic', username: 'user', password: 'pass' } }
+    { auth: { type: "basic", username: "user", password: "pass" } }
   );
 });
 
-test('optionsFromEnvironment token', t => {
+test("optionsFromEnvironment token", t => {
   t.deepEqual(
     BitbucketProvider.optionsFromEnvironment({
-      BB_TOKEN: '1234'
+      BB_TOKEN: "1234"
     }),
-    { auth: { type: 'token', token: '1234' } }
+    { auth: { type: "token", token: "1234" } }
   );
   t.deepEqual(
     BitbucketProvider.optionsFromEnvironment({
-      BITBUCKET_TOKEN: '1234'
+      BITBUCKET_TOKEN: "1234"
     }),
-    { auth: { type: 'token', token: '1234' } }
+    { auth: { type: "token", token: "1234" } }
   );
 });
 
-test('api url', t => {
+test("api url", t => {
   t.deepEqual(
-    { '2.0': 'https://api.bitbucket.org/2.0' },
-    BitbucketProvider.apiURLs('https://bitbucket.org', '2.0')
+    { "2.0": "https://api.bitbucket.org/2.0" },
+    BitbucketProvider.apiURLs("https://bitbucket.org", "2.0")
   );
 });
 
-test('api url stash', t => {
+test("api url stash", t => {
   t.deepEqual(
-    { '1.0': 'https://api.stash.mydomain.com/1.0' },
-    BitbucketProvider.apiURLs('https://stash.mydomain.com')
+    { "1.0": "https://api.stash.mydomain.com/1.0" },
+    BitbucketProvider.apiURLs("https://stash.mydomain.com")
   );
 });
 
-test('provider', async t => {
+test("provider", async t => {
   const provider = new BitbucketProvider(config);
   const repository = await provider.repository(REPOSITORY_NAME);
 
   t.is(repository.name, REPOSITORY_NAME);
   t.is(
-    repository.urls.find(u => u.startsWith('http')),
-    'https://bitbucket.org/arlac77/sync-test-repository.git'
+    repository.urls.find(u => u.startsWith("http")),
+    "https://bitbucket.org/arlac77/sync-test-repository.git"
   );
 
   const branches = await repository.branches();
-  t.is(branches.get('master').name, 'master');
+  t.is(branches.get("master").name, "master");
 
-  const branch = await repository.branch('master');
-  t.is(branch.name, 'master');
+  const branch = await repository.branch("master");
+  t.is(branch.name, "master");
 });
 
-test('provider repository undefined', async t => {
+test("provider repository undefined", async t => {
   const provider = new BitbucketProvider(config);
   const repository = await provider.repository(undefined);
 
   t.is(repository, undefined);
 });
 
-test('provider url git+https mybitbucket.org', async t => {
-  const provider = new BitbucketProvider({ url: 'https://mybitbucket.org' });
+test("provider url git+https mybitbucket.org", async t => {
+  const provider = new BitbucketProvider({ url: "https://mybitbucket.org" });
 
   const repository = await provider.repository(
-    'git+https://arlac77@mybitbucket.org/arlac77/sync-test-repository.git'
+    "git+https://arlac77@mybitbucket.org/arlac77/sync-test-repository.git"
   );
 
-  t.is(repository.name, 'arlac77/sync-test-repository');
-  t.is(repository.user, 'arlac77');
+  t.is(repository.name, "arlac77/sync-test-repository");
+  t.is(repository.user, "arlac77");
 
-  const branch = await repository.branch('master');
-  t.is(branch.name, 'master');
+  const branch = await repository.branch("master");
+  t.is(branch.name, "master");
 });
 
-test('provider url git+https', async t => {
+test("provider url git+https", async t => {
   const provider = new BitbucketProvider(config);
   const repository = await provider.repository(
-    'git+https://arlac77@bitbucket.org/arlac77/sync-test-repository.git'
+    "git+https://arlac77@bitbucket.org/arlac77/sync-test-repository.git"
   );
 
-  t.is(repository.name, 'arlac77/sync-test-repository');
-  t.is(repository.user, 'arlac77');
+  t.is(repository.name, "arlac77/sync-test-repository");
+  t.is(repository.user, "arlac77");
 
-  const branch = await repository.branch('master');
-  t.is(branch.name, 'master');
+  const branch = await repository.branch("master");
+  t.is(branch.name, "master");
 });
 
-test('provider url https with user', async t => {
+test("provider url https with user", async t => {
   const provider = new BitbucketProvider(config);
   const repository = await provider.repository(
-    'https://arlac77@bitbucket.org/arlac77/sync-test-repository.git'
+    "https://arlac77@bitbucket.org/arlac77/sync-test-repository.git"
   );
 
-  t.is(repository.name, 'arlac77/sync-test-repository');
-  t.is(repository.user, 'arlac77');
+  t.is(repository.name, "arlac77/sync-test-repository");
+  t.is(repository.user, "arlac77");
 
-  const branch = await repository.branch('master');
-  t.is(branch.name, 'master');
+  const branch = await repository.branch("master");
+  t.is(branch.name, "master");
 });
 
-test('provider url https with user and password', async t => {
+test("provider url https with user and password", async t => {
   const provider = new BitbucketProvider(config);
   const repository = await provider.repository(
-    'https://arlac77:aSecret@bitbucket.org/arlac77/sync-test-repository.git'
+    "https://arlac77:aSecret@bitbucket.org/arlac77/sync-test-repository.git"
   );
 
-  t.is(repository.name, 'arlac77/sync-test-repository');
-  t.is(repository.user, 'arlac77');
+  t.is(repository.name, "arlac77/sync-test-repository");
+  t.is(repository.user, "arlac77");
 
-  const branch = await repository.branch('master');
-  t.is(branch.name, 'master');
+  const branch = await repository.branch("master");
+  t.is(branch.name, "master");
 });
 
-test('provider url ssh://git@bitbucket.org/arlac77...', async t => {
+test("provider url ssh://git@bitbucket.org/arlac77...", async t => {
   const provider = new BitbucketProvider(config);
   const repository = await provider.repository(
-    'ssh://git@bitbucket.org/arlac77/sync-test-repository.git'
+    "ssh://git@bitbucket.org/arlac77/sync-test-repository.git"
   );
 
-  t.is(repository.name, 'arlac77/sync-test-repository');
-  t.is(repository.user, 'arlac77');
+  t.is(repository.name, "arlac77/sync-test-repository");
+  t.is(repository.user, "arlac77");
 
-  const branch = await repository.branch('master');
-  t.is(branch.name, 'master');
+  const branch = await repository.branch("master");
+  t.is(branch.name, "master");
 });
 
-test('provider url git@', async t => {
+test("provider url git@", async t => {
   const provider = new BitbucketProvider(config);
   const repository = await provider.repository(
-    'git@bitbucket.org:arlac77/sync-test-repository.git'
+    "git@bitbucket.org:arlac77/sync-test-repository.git"
   );
 
-  t.is(repository.name, 'arlac77/sync-test-repository');
-  t.is(repository.user, 'arlac77');
+  t.is(repository.name, "arlac77/sync-test-repository");
+  t.is(repository.user, "arlac77");
 
-  const branch = await repository.branch('master');
-  t.is(branch.name, 'master');
+  const branch = await repository.branch("master");
+  t.is(branch.name, "master");
 });
 
-test('provider url git@ invalid', async t => {
+test("provider url git@ invalid", async t => {
   const provider = new BitbucketProvider(config);
   t.is(
     await provider.repository(
-      'git@bitbucket.org/arlac77/sync-test-repository.git'
+      "git@bitbucket.org/arlac77/sync-test-repository.git"
     ),
     undefined
   );
 });
 
-test('provider url git+ssh@', async t => {
+test("provider url git+ssh@", async t => {
   const provider = new BitbucketProvider(config);
   const repository = await provider.repository(
-    'git+ssh@bitbucket.org:arlac77/sync-test-repository.git'
+    "git+ssh@bitbucket.org:arlac77/sync-test-repository.git"
   );
 
-  t.is(repository.name, 'arlac77/sync-test-repository');
-  t.is(repository.user, 'arlac77');
+  t.is(repository.name, "arlac77/sync-test-repository");
+  t.is(repository.user, "arlac77");
 
-  const branch = await repository.branch('master');
-  t.is(branch.name, 'master');
+  const branch = await repository.branch("master");
+  t.is(branch.name, "master");
 });
 
-test('provider repo with branch name', async t => {
+test("provider repo with branch name", async t => {
   const provider = new BitbucketProvider(config);
 
-  const branch = await provider.branch(REPOSITORY_NAME + '#master');
+  const branch = await provider.branch(REPOSITORY_NAME + "#master");
 
   t.is(branch.provider, provider);
-  t.is(branch.name, 'master');
+  t.is(branch.name, "master");
   //t.is(branch.user, 'arlac77');
 });
 
-test('list', async t => {
+test("list", async t => {
   const provider = new BitbucketProvider(config);
   const branch = await provider.branch(REPOSITORY_NAME);
 
-  t.deepEqual(await branch.list(), [{ path: 'README.md' }]);
+  t.deepEqual(await branch.list(), [{ path: "README.md" }]);
 });
 
-test('content', async t => {
+test("content", async t => {
   const provider = new BitbucketProvider(config);
   const branch = await provider.branch(REPOSITORY_NAME);
 
-  const c = await branch.content('README.md');
+  const c = await branch.content("README.md");
 
-  t.is(c.path, 'README.md');
-  t.is(c.content.startsWith('# README'), true);
+  t.is(c.path, "README.md");
+  t.is(c.content.startsWith("# README"), true);
 });
