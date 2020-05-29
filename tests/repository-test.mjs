a9daf22763655d8fad8621ac93b2b00601476c98
Markus Felten <Markus.Felten@gmx.de>
2020-05-29 11:42:16 +0200

    fix: bump deps

--- tests/repository-test.mjs
+++ tests/repository-test.mjs
@@ -57,17 +57,17 @@ const repoFixtures = {
     entries: { "package.json": {}, ".jsdoc.json": {} }
   },
 
-  "https://bitbucket.org/arlac77/template-cli-app.git": {
+  "https://bitbucket.org/arlac77/template-node-app.git": {
     provider: BitbucketProvider,
-    name: "template-cli-app",
+    name: "template-node-app",
     uuid: "{bec21095-03ca-45ad-8571-b7d611a6dffd}",
     //  uuid: '{36734289-3058-4c37-86ff-0ee8696d3d9d}',
     branch: "master"
   },
-  "https://arlac77@bitbucket.org/arlac77/template-cli-app.git": {
+  "https://arlac77@bitbucket.org/arlac77/template-node-app.git": {
     provider: BitbucketProvider,
-    name: "template-cli-app",
-    fullName: "arlac77/template-cli-app",
+    name: "template-node-app",
+    fullName: "arlac77/template-node-app",
     uuid: "{bec21095-03ca-45ad-8571-b7d611a6dffd}",
     //uuid: '{36734289-3058-4c37-86ff-0ee8696d3d9d}',
     owner: owner1,
@@ -84,7 +84,7 @@ const repoFixtures = {
 };
 
 test("locate repository several", async t => {
-  t.plan(644);
+  t.plan(647);
 
   const provider = BitbucketProvider.initialize(undefined, process.env);
 
