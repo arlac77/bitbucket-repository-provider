35e0cd911ffef35f745d0c4902dfc0696deefcc7
Markus Felten <Markus.Felten@gmx.de>
2020-06-13 21:40:01 +0200

    fix: attribute streamelinining

--- src/bitbucket-repository-group.mjs
+++ src/bitbucket-repository-group.mjs
@@ -4,13 +4,12 @@ import { RepositoryGroup } from "repository-provider";
  *
  */
 export class BitbucketRepositoryGroup extends RepositoryGroup {
-  static get attributeMappiong() {
+  static get attributeMapping() {
     return {
       ...super.attributeMapping,
       display_name: "displayName",
-
-      //"links.avatar.href": "avatarURL"
-      //website: "homePageURL"
+      "links.avatar.href": "avatarURL",
+      website: "homePageURL"
     };
   }
 }
