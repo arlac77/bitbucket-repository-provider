50ff208b38206a3b5212a649ff600847e1582eb5
Markus Felten <Markus.Felten@gmx.de>
2020-11-27 20:22:46 +0100

    wip: create repo

--- src/bitbucket-repository-group.mjs
+++ src/bitbucket-repository-group.mjs
@@ -12,4 +12,30 @@ export class BitbucketRepositoryGroup extends RepositoryGroup {
       website: "homePageURL"
     };
   }
+
+  /**
+   * {@link https://community.atlassian.com/t5/Bitbucket-articles/Create-and-configure-a-Bitbucket-Server-repository-using-the/ba-p/828364}
+   * @param {string} name
+   * @param {Object} options
+   * @return {Repository} newly created repository
+   */
+  async createRepository(name, options = {}) {
+    //console.log(`repositories/${this.name}/${name}`);
+    const response = await this.provider.fetch(
+      `repositories/${this.name}/${name}`,
+      {
+        method: "POST",
+        body: JSON.stringify({
+          name,
+          ...options
+        })
+      }
+    );
+
+    //console.log(response);
+
+    if (response.ok) {
+      return this.addRepository(name, options);
+    }
+  }
 }
--- tests/repository-livecycle-test.mjs
+++ tests/repository-livecycle-test.mjs
@@ -2,10 +2,14 @@ import test from "ava";
 import { repositoryLivecycleTest } from "repository-provider-test-support";
 import BitbucketProvider from "bitbucket-repository-provider";
 
-test("create & delete repo", async t =>
+test.skip("create & delete repo", async t =>
   repositoryLivecycleTest(
     t,
     BitbucketProvider.initialize(undefined, process.env),
     "test-repo-1",
-    "arlac77"
+    "arlac77",
+    { description: "a description" },
+    async (t, repository) => {
+      t.is(repository.description, "a description", "description");
+    }
   ));
