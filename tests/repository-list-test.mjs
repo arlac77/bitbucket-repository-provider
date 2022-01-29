7604697c926afb0887f9dd4ba0a4497c662819fa
Markus Felten <Markus.Felten@gmx.de>
2022-01-30 00:53:39 +0100

    test: empty filter -> all

--- tests/repository-list-test.mjs
+++ tests/repository-list-test.mjs
@@ -4,7 +4,6 @@ import BitbucketProvider from "bitbucket-repository-provider";
 
 const provider = BitbucketProvider.initialize(undefined, process.env);
 
-
 const xhubioRepos = {
   "xhubio/decision-table-data-generator": {
     name: "decision-table-data-generator"
@@ -18,4 +17,3 @@ test(repositoryListTest, provider, "https://bitbucket.org/xhubio/*", xhubioRepos
 test(repositoryListTest, provider, "unknown:xhubio/*");
 test(repositoryListTest, provider, "xhubio/invalid_repository_name");
 test(repositoryListTest, provider, "invalid_user_name/*");
-test(repositoryListTest, provider, "");
