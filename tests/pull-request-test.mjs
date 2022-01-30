6808066e5a5088bcb638e07f851815f628c8b945
Markus Felten <Markus.Felten@gmx.de>
2022-01-30 01:08:26 +0100

    test: use BITBUCKET_REPOSITORY_NAME

--- tests/pull-request-test.mjs
+++ tests/pull-request-test.mjs
@@ -1,12 +1,12 @@
 import test from "ava";
-import { pullRequestLivecycle, pullRequestList, REPOSITORY_NAME } from "repository-provider-test-support";
+import { pullRequestLivecycle, pullRequestList, BITBUCKET_REPOSITORY_NAME } from "repository-provider-test-support";
 import BitbucketProvider from "bitbucket-repository-provider";
 
 test("pr livecycle", async t => {
   await pullRequestLivecycle(
     t,
     BitbucketProvider.initialize(undefined, process.env),
-    REPOSITORY_NAME
+    BITBUCKET_REPOSITORY_NAME
   );
 });
 
@@ -14,6 +14,6 @@ test("pr list", async t => {
   await pullRequestList(
     t,
     BitbucketProvider.initialize(undefined, process.env),
-    REPOSITORY_NAME
+    BITBUCKET_REPOSITORY_NAME
   );
 });
